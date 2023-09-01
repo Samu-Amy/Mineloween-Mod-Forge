2ecaa2d6a56c43b0f9d3cb84da1f15767f3e6afb
Samuele Amisano <65444098+Samu-Amy@users.noreply.github.com>
2023-09-01 19:42:49 +0200

    Tags

--- Mineloween Mod/src/main/java/net/samu/mineloween/item/custom/WandItem.java
+++ Mineloween Mod/src/main/java/net/samu/mineloween/item/custom/WandItem.java
@@ -13,11 +13,16 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Snowball;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.samu.mineloween.util.ModTags;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class WandItem extends Item {
     public WandItem(Properties pProperties) {
@@ -52,15 +57,22 @@ public class WandItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        pTooltipComponents.add(Component.translatable("tooltip.samu_mineloween_mod.wand.tooltip"));
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.COAL_ORE);
+        return state.is(ModTags.Blocks.WAND_VALUABLES);
     }
 
     private void outputValuableCoordinates(BlockPos blockPos, Player player, Block block) {
-        player.sendSystemMessage(Component.literal("Found" + I18n.get(block.getDescriptionId())
+        player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId())
                 + " at " + "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"));
     }
 
+    // Snowball
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pHand) {
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         pLevel.playSound((Player)null, pPlayer.getX(), pPlayer.getY(), pPlayer.getZ(), SoundEvents.SNOWBALL_THROW, SoundSource.NEUTRAL, 0.5F, 0.4F / (pLevel.getRandom().nextFloat() * 0.4F + 0.8F));
---
+++ Mineloween Mod/src/main/java/net/samu/mineloween/util/ModTags.java
@@ -0,0 +1,27 @@
+package net.samu.mineloween.util;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.samu.mineloween.SamuMineloweenMod;
+
+// TODO: Elimina o modifica (per trovare i mob interessati nel raggio d'azione) "WAND_VALUABLES"
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> WAND_VALUABLES= tag("wand_valuables");
+
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(SamuMineloweenMod.MOD_ID, name));
+        }
+    }
+
+    public static class Items {
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(SamuMineloweenMod.MOD_ID, name));
+        }
+    }
+}
